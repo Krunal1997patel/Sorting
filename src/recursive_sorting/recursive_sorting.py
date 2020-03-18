292d4db9989485e33826c95132d5ea1ad1ca58ea
Krunal Patel <Pkrunal616@gmail.com>
2020-03-18 16:51:09 -0400

    merge sorting

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,18 +1,71 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
+def merge(arrA, arrB):
+    merged_arr = []
+    i = 0
+    j = 0
     # TO-DO
-    
+
+    while i < len(arrA) and j < len(arrB):
+        if arrA[i] < arrB[j]:
+            merged_arr.append(arrA[i])
+            i += 1
+        else:
+            merged_arr.append(arrB[j])
+            j += 1
+
+    while i < len(arrA):
+        merged_arr.append(arrA[i])
+        i += 1
+
+    while j < len(arrB):
+        merged_arr.append(arrB[j])
+        j += 1
+
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
+def merge_sort(arr):
     # TO-DO
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[0:mid])
+    right = merge_sort(arr[mid:])
 
-    return arr
+    return merge(left, right)
 
+
+print(
+    merge_sort(
+        [
+            123,
+            4,
+            2,
+            42,
+            31,
+            3,
+            234,
+            1,
+            23,
+            23,
+            443,
+            523,
+            55,
+            463,
+            45,
+            345,
+            4356,
+            5,
+            52,
+            4,
+            234,
+            345,
+            43,
+            3,
+        ]
+    )
+)
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
@@ -20,7 +73,8 @@ def merge_in_place(arr, start, mid, end):
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
@@ -28,6 +82,6 @@ def merge_sort_in_place(arr, l, r):
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort(arr):
 
     return arr
